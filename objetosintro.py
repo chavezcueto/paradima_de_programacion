cc3cdc1c67dc4658c6b28cc8ab450bb30e76006c
chavezcueto <jare1852@gmail.com>
2023-10-03 15:53:00 -0600

    objetosintro

--- objetosintro.py
+++ objetosintro.py
@@ -20,7 +20,7 @@ nada= objetovacio()
 print(type(nada))
 
 #==== la clase llanta ====
-class llanta:
+class Llanta:
     #==== variabl cuenta es de toda la clase ====
     cuenta = 0
     #==== funcion constructora de identidad ====
@@ -28,19 +28,19 @@ class llanta:
     #==== comienza con uno mismo: self ====
     #==== prametro de entrada = default ====
 
-    def __int__(mi,radio=50,ancho=30,presion=1.5):
+    def __init__(mi,radio=50,ancho=30,presion=1.5):
         #variable de la esctructura completa llanta
-        llanta.cuenta +=1
+        Llanta.cuenta +=1
         #variable exclusivas de cada objeto
         mi.radio = radio
         mi.ancho = ancho
         mi.presion = presion 
 
 #==== objetos (instanciados) ====
-llanta1 = llanta(50,30,1.5)
-llanta2 = llanta(presion=1.2)
-llanta3 = llanta()
-llanta4 = llanta(40,30,1.6)
+llanta1 = Llanta(50,30,1.5)
+llanta2 = Llanta(presion=1.2)
+llanta3 = Llanta()
+llanta4 = Llanta(40,30,1.6)
 
 #==== objetos que contiene otros objetos ====
 
@@ -52,8 +52,7 @@ class coche:
         mi.llanta4=ll4
 
 micoche = coche(llanta1,llanta2,llanta3,llanta4)
-
-print("total de llanta: ",llanta.cuenta) #variable global de la clase
+print("total de llanta: ",Llanta.cuenta) #variable global de la clase
 print("presion de la llanta 4 = ", llanta4.presion) #presion de la llanta 4
 print("radio de la llanta 4 = ",llanta4.radio)
 print("radio de la llanta 3 = ",llanta3.radio)
@@ -62,31 +61,83 @@ print("presion de la llanat 1 de mi coche = ", micoche.llanta1.presion)
  #==== encapsulamiento ====
 
  #==== uso de la funcion de python property para poner y obtener atributos ====
- class estudiantes:
-     def __init__ (mi):
-         mi._nombre = " "
-        def ponerme_nombre(mi, nombre):
+class Estudiante:
+    def __init__ (mi):
+         mi.__nombre = " "
+    def ponerme_nombre(mi, nombre):
             print ("se llamo a ponerme_nombre")
-            mi._nombre=nombre
-        def obtener_nombre(mi):
+            mi.__nombre=nombre
+    def obtener_nombre(mi):
             print("se llamo a obtener_nombre")
-            return mi._nombre
-        nombre=property(obtener_nombre,ponerme_nombre)
+            return mi.__nombre
+    nombre=property(obtener_nombre,ponerme_nombre)
  
  #==== crear objeto estudiante sin nombre ====
- Estudiante= estudiante()
+estudiante= Estudiante()
  
  #==== ponerle nombre usando la propiedad nombre y el metodo ponerme_nombre =====
  #==== (sin tener que llamar explicitamente la funcion)====
 
- estudiante.nombre = "diego"
+estudiante.nombre = "diego"
 
  #==== obtener el nombre con el metodo obtener_nombre ====
  #==== __nombre es una variable encapsulada (no visible desde afuera) ====
  #==== (sin tener que lñlamar explicitamente a la funcion obtener_nombre) ====
- print(estudiante.nombre)
+print(estudiante.nombre)
   
 #==== eesto no funciona ====
-#print(estiante.__nombre)
+#print(estudiante.__nombre)
 
-#==== herencia ====
+#==== herencia de clases ====
+
+class Cuadrilatero:
+    def __init__(mi, a, b, c, d):
+        mi.lado1=a
+        mi.lado2=b
+        mi.lado3=c
+        mi.lado4=d
+
+    def perimetro(mi):
+        p=mi.lado1 + mi.lado2 + mi.lado3 + mi.lado4
+        print("perimetro= ",p)
+        return p
+
+# ==== su hijo rctangulo [rectangulo es hijo de cuadrilatero(rectangulo(cuadrilatero))
+
+class Rectangulo(Cuadrilatero):
+    def __init__(self, a, b):
+        #==== constructor de su madre ====
+        super().__init__(a,b,a,b)
+
+#==== su nieto cuadrado (hijo de rectangulo)
+
+class Cuadrado(Rectangulo):
+    def __init__(self,a):
+        super().__init__(a,a)
+
+    def area(self):
+        area = self.lado1**2
+        return area
+
+    #def perimetro(self):
+    # p=4.0*self.lado1
+    # print("perimetro= ",p)
+    # return p
+
+#==== crear un cuadrado ====
+
+cuadrado1 = Cuadrado(5)
+
+#==== llamar al metodo perimetro de su abuelo cuadrilatero ====
+perimetro1 = cuadrado1.perimetro()
+
+#==== llamar a su propio metodo area ====
+
+area1 = cuadrado1.area()
+
+print("perimetro= ",perimetro1)
+print("area= ", area1)
+
+#==== sobre-escribir un metodo de su madre o abuela o tatarabuela... =====
+#==== es volver a definir una funcion ya existente ====
+
