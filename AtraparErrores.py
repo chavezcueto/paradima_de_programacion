45513f809b7d554574e8e60a2291da6b2f94c69a
chavezcueto <jare1852@gmail.com>
2023-10-19 19:01:44 -0600

    atraparerrores

--- AtraparErrores.py
+++ AtraparErrores.py
@@ -8,7 +8,7 @@
 
 #==== La clase ClienteBancario esta en el subdirectorio aplicacion/banco ====
 
-from aplicacion.banco.cliente_bancario import ClienteBancario
+from cliente_bancario import ClienteBancario
 
 # try: intenta (correr las instrucciones)
 # except: atrapar el error en una variable e
@@ -16,22 +16,22 @@ from aplicacion.banco.cliente_bancario import ClienteBancario
 
 #==== Error por sacar mas dinero del que tiene ====
 try:
-	cliente= ClienteBancario("Jaretzy Yajahira","Chavez Cueto",21,0.0)
-	cliente.guardarDinero(300)
-	print(cliente.imprimirInfo())
-	cliente.retirarDinero(400)
-	print(cliente.imprimirInfo())
+    cliente= ClienteBancario("Jaretzy Yajahira","Chavez Cueto",21,0.0)
+    cliente.guardarDinero(300)
+    print(cliente.imprimirInfo())
+    cliente.retirarDinero(400)
+    print(cliente.imprimirInfo())
 
 # Exception es el objeto mas general de error
 
 except Exception as e:
-	print("Error: " + str(e)) 
+    print("Error: " + str(e)) 
 
 #==== Error por uszsar un artributo privado ====
 try:
-	print(cliente.__nombres)
+    print(cliente.nombres)
 except Exception as ex:
-	print("Error: " + str(ex))
+    print("Error: " + str(ex))
 
 #==== Fornma correcta ====
-print(cliente_nombres)
+print(cliente.nombres)
