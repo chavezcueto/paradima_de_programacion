da6c3ad0e6c4a73a4a2d465cf48454b4e1d684a3
chavezcueto <jare1852@gmail.com>
2023-11-30 15:53:35 -0600

    30_11_23

--- programas_principales/calor2D.py
+++ programas_principales/calor2D.py
@@ -42,4 +42,55 @@ print("dt = ",dt)
 nt = n[0]*n[1]
 print("celdas = ", nt)
 
-#
+#===============================
+# Arreglos iniciales
+#===============================
+
+# llenar la solucion con ceros
+u = np.zeros(nt,dtype=np.float64) #arreglo de lectura
+un = np.zeros(nt.dtype=np.float64) # arreglo de escritura
+
+#==============================================================
+# evolucion temporal de la ecucion diferencial parcial
+# u_t = k*laplaciano(u) (ecucion de difunsion de calor)
+#==============================================================
+
+def evolucion(u,n,udx2,dt,i,k):
+    jpl = i + n[0]
+    jml = i - n[0]
+    laplaciano = (u[i-1]-2.0*u[i]+u[i+1])*udx2[0] + (u[jml]-2.0*u[i]+u[jpl])*udx2[1]
+    unueva = i[i] + dt*k*laplaciano
+    return unueva
+
+#==================================================================
+# loop sobre toda la malla para avanzar la ecucion en el tiempo 
+# no contien la frontera (u=0 en toda la orilla del dominio)
+#==================================================================
+
+def solucion(u,un,udx2,dt,n,k):
+    for jj in range(1,n[1]-1):
+        for ii in range(1,n[0]-1):
+            i = ii + n[0]*jj
+            #====================================
+            # avanzar la ecuacion en un punto
+            #====================================
+            unueva = evolucion(u,n,udx2,dt,i,k)
+            #====================================================
+            # en medio de la malla poner temperatura fija 1
+            #====================================================
+            if i == int(nt/2)+int(n[0]/2):
+                unueva = 1.0
+            un[i] = unueva
+
+#============================
+# PROGRAMA PRINCIPAL
+#============================
+
+start = time.time()
+
+#=====================
+# pasos de tiempo
+#=====================
+
+for t in range(1,pasos+1):
+
